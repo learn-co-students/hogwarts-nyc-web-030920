25ee7c73df1f6a801fa6cf2bc47afda007f4c8aa
MisterC1ean <robert.keller22@gmail.com>
2020-05-14 13:33:43 -0400

    added images to hogCard

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,35 +1,49 @@
 import React from "react";
 
 export default class HogCard extends React.Component {
-    state = {
-        isFront: true,
-        hidden: false
-    }
+	state = {
+		isFront: true,
+		hidden: false,
+	};
 
-    flipCard = () => {
-        this.setState({isFront: !this.state.isFront})
-    }
+	flipCard = () => {
+		this.setState({ isFront: !this.state.isFront });
+	};
 
-    hideHog = () => {
-        this.setState({hidden: !this.state.hidden})
-    }
+	hideHog = () => {
+		this.setState({ hidden: !this.state.hidden });
+	};
+	getImg = () => {
+        let pigName = this.props.name
+        let split = pigName.split(" ")
+        let imgName = split.join("_").toLowerCase()
+		let pigImage = require(`../hog-imgs/${imgName}.jpg`); //how can we dynamically generate the filename?
+		return <img alt={this.props.name} src={pigImage} />;
+	};
 	render() {
-        // console.log(this.state)
-        const className = this.state.hidden ? "hidden" : "card"
-		return(
-            <div className={className} onClick={this.flipCard}>
-                {this.state.isFront
-                    ? <div>{this.props.name}</div>
-                    : (<div>
-                        <div>Specialty: {this.props.specialty}</div>
-                        <div>Weight: {this.props.weight}</div>
-                        <div>Greased: {this.props.greased ? "YES" : "NO"}</div>
-                        <div>Highest Medal Achieved: {this.props['highest medal achieved']}</div>
-                    </div>)
-                }
-                <br/>
-                <button onClick={this.hideHog}>Hide Hog</button>
-            </div>
-        )
+		// console.log(this.state)
+		const className = this.state.hidden ? "hidden" : "card";
+		return (
+			<div className={className} onClick={this.flipCard}>
+				{this.state.isFront ? (
+					<div>
+						<div>{this.props.name}</div>
+						<div>{this.getImg()}</div>
+					</div>
+				) : (
+					<div>
+						<div>Specialty: {this.props.specialty}</div>
+						<div>Weight: {this.props.weight}</div>
+						<div>Greased: {this.props.greased ? "YES" : "NO"}</div>
+						<div>
+							Highest Medal Achieved:{" "}
+							{this.props["highest medal achieved"]}
+						</div>
+					</div>
+				)}
+				<br />
+				<button onClick={this.hideHog}>Hide Hog</button>
+			</div>
+		);
 	}
 }
